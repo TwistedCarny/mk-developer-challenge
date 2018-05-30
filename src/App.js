fb8098a00cea6959ffeb935f919608dd7df1fa4f
Westin <westincurtis@gmail.com>
2018-05-29 21:19:25 -0400

    Basic form that submits to amazon API gateway

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+
+        const data = {name: this.state.fullName, email: this.state.email, subject: "MK Developer Challenge", message: this.state.message, 'Content-Type': 'application/json'};
+        fetch("https://len332mac1.execute-api.us-east-1.amazonaws.com/Testing/emails", {method: "post", body: JSON.stringify(data)}).then(function (response) {
+            console.log("successfully sent the email");
+        }).catch( function (err) {
+            console.log("Whoops! Something happened.")
+        });
+
+    }
+
+    handleChange(event){
+        const name = event.target.name;
+        this.setState({[name]: event.target.value});
+    }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form onSubmit={this.handleSubmit}  method="post">
+        <TextField id="fullName" name="fullName" label="Name" placeholder="Full Name" fullWidth margin="normal" required  onChange={this.handleChange}/>
+          <TextField id="email" name="email" label="Email" placeholder="Email Address" fullWidth type="email" margin="normal" required  onChange={this.handleChange}/>
+          <TextField id="message" name="message" label="Message" multiline rows="6" fullWidth placeholder="Enter your message." margin="normal" required onChange={this.handleChange}/>
+          <Button variant="raised" color="primary" fullWidth type="submit">Submit</Button>
+            <Typography>{this.state.responseMessage}</Typography>
+        </form>
       </div>
     );
   }
